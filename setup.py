07f7ac7a28c7222a39d04b239ed55d4f535a4cc4
SamuelGabriel <Samuel.g.mueller@web.de>
2023-11-29 12:24:29 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="caafe",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     description="Context-Aware Automated Feature Engineering (CAAFE) is an automated machine learning tool that uses large language models for feature engineering in tabular datasets. It generates Python code for new features along with explanations for their utility, enhancing interpretability.",
     long_description=open("README.md").read(),
